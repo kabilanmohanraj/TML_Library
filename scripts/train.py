f990e9b4eb227f7a21ab939453d99c609a926074
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2024-12-02 15:41:10 -0800

    remove logger, use print

--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,16 @@
 # scripts/train.py
-from tml_library import Trainer, initialize_train_pipeline
+from tml_library import Trainer, initialize_train_pipeline, Evaluator
 
 # Initialize components
-X, y, model, param_grid, logger = initialize_train_pipeline("./configs/config.json")
+X_train, X_test, y_train, y_test, model, param_grid = initialize_train_pipeline("./configs/config.json")
+print("Model training...")
 
 # Train and save the model
 trainer = Trainer(model, param_grid)
-best_model = trainer.train(X, y)
+best_model = trainer.train(X_train, y_train)
 trainer.save_model("./best_model.joblib")
-logger.info("Model training complete and saved.")
+print("Model training complete and saved.")
+
+evaluator = Evaluator(best_model)
+metrics = evaluator.evaluate(X_test, y_test)
+print(f"Evaluation Metrics:\n{metrics}")
